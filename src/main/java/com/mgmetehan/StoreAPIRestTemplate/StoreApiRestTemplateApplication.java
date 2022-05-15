cf1bff35f43d664e6a82097746f03af8ebdae974
Metehan Gültekin <mgmetehan@hotmail.com>
2022-05-15 11:47:20 +0300

    first commit

---
+++ src/main/java/com/mgmetehan/StoreAPIRestTemplate/StoreApiRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.StoreAPIRestTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApiRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApiRestTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgmetehan/StoreAPIRestTemplate/StoreApiRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.StoreAPIRestTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
